a170157199d8483c62353a820d13350bc846fbee
Afonso Garcia <afonsogarcia@icloud.com>
2015-04-02 09:30:32 +0100

    V2.1

---
+++ src/main/java/DirectionReturn.java
@@ -0,0 +1,10 @@
+import cern.ais.gridwars.Coordinates;
+import cern.ais.gridwars.command.MovementCommand;
+
+/**
+ * Created by agfrg on 02/04/15.
+ */
+public class DirectionReturn {
+    public MovementCommand.Direction dir;
+    public Double dist;
+}
--- src/main/java/FABot.java
+++ src/main/java/FABot.java
@@ -10,14 +10,28 @@ import java.util.Random;
  * Created by agfrg on 01/04/15.
  */
 public class FABot implements PlayerBot {
+    Coordinates adversaryFirst;
+    Boolean firstRound = true;
+    Boolean unleashHell = false;
     @Override
     public void getNextCommands(UniverseView universeView, List<MovementCommand> movementCommands) {
+        if(firstRound) {
+            adversaryFirst = getAdversaryFirst(universeView);
+            firstRound = false;
+        }
+
         List<Coordinates> myCoord = universeView.getMyCells();
 
         for(Coordinates coord : myCoord) {
-            if(universeView.getPopulation(coord) >= 2) {
+            if(universeView.getPopulation(coord) >= 2 && unleashHell) {
                 Long movement = universeView.getPopulation(coord) / 2;
                 movementCommands.add(new MovementCommand(coord, getRandomDirection(), movement));
+            } else if (universeView.getPopulation(coord) >= 2) {
+                DirectionReturn dir = getClosestDirection(coord);
+                if(dir.dist < 5) {
+                    unleashHell = true;
+                }
+                movementCommands.add(new MovementCommand(coord, dir.dir, universeView.getPopulation(coord)));
             }
         }
     }
@@ -32,5 +46,69 @@ public class FABot implements PlayerBot {
             default: return MovementCommand.Direction.RIGHT;
 
         }
+    }
+
+    private DirectionReturn getClosestDirection(Coordinates coord) {
+        Double distance = getDistance(coord, adversaryFirst);
+        DirectionReturn ret = null;
+
+        Double leftDistance = getDistance(coord.getLeft(), adversaryFirst);
+        if (leftDistance < distance) {
+            distance = leftDistance;
+            ret = new DirectionReturn();
+            ret.dir = MovementCommand.Direction.LEFT;
+            ret.dist = distance;
+        }
+
+        Double rightDistance = getDistance(coord.getRight(), adversaryFirst);
+        if (rightDistance < distance) {
+            distance = rightDistance;
+            ret = new DirectionReturn();
+            ret.dir = MovementCommand.Direction.RIGHT;
+            ret.dist = distance;
+        }
+
+        Double upDistance = getDistance(coord.getUp(), adversaryFirst);
+        if (upDistance < distance) {
+            distance = upDistance;
+            ret = new DirectionReturn();
+            ret.dir = MovementCommand.Direction.UP;
+            ret.dist = distance;
+        }
+
+        Double downDistance = getDistance(coord.getDown(), adversaryFirst);
+        if (downDistance < distance) {
+            distance = downDistance;
+            ret = new DirectionReturn();
+            ret.dir = MovementCommand.Direction.DOWN;
+            ret.dist = distance;
+        }
+
+        if(ret == null) {
+            throw new RuntimeException();
+        } else {
+            return ret;
+        }
+    }
+
+    private Double getDistance(Coordinates x, Coordinates y) {
+        Integer xCoord = Math.abs(x.getX() - y.getX());
+        Integer yCoord = Math.abs(x.getY() - y.getY());
+        return Math.sqrt(Math.pow(xCoord, 2) + Math.pow(yCoord, 2));
+    }
+
+    private Coordinates getAdversaryFirst(UniverseView universeView) {
+        int universeSide = universeView.getUniverseSize();
+
+        for (int i = 0; i < universeSide; i++) {
+            for (int j = 0; j < universeSide; j++) {
+                Coordinates coord = universeView.getCoordinates(i, j);
+                if(!(universeView.isEmpty(coord) || universeView.belongsToMe(coord))) {
+                    return coord;
+                }
+            }
+        }
+
+        throw new RuntimeException();
     }
 }
